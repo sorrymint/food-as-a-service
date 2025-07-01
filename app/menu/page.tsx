225bd50dff1c660b3cbe312f3732c0873f97388b
Ashley2n <a.windbreacker@gmail.com>
2025-07-01 10:47:01 -0500

    Synching changings to remote

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -6,6 +6,12 @@ import { ShoppingCart, X } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import Header from 'components/ui/header'
+import Link from 'next/link';
+import Image from 'next/image';
+import { db } from '@/lib/db/drizzle';
+import { dishes } from '@/lib/db/schema';
+
 
 interface MenuItem {
     id: number;
@@ -20,6 +26,51 @@ interface MenuItem {
 interface CartItem extends MenuItem {
     quantity: number;
 }
+
+// import Header from 'components/ui/header'
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import { db } from '@/lib/db/drizzle';
+// import { dishes } from '@/lib/db/schema';
+
+
+// export default async function menu() {
+
+//   const menuItems = await db.select().from(dishes);
+//   return (
+//     <div className='flex flex-col gap-12'>
+//       <div className='flex flex-col gap-4'>
+//       <h1 className='text-3xl mx-auto '>Menu</h1>
+//       <Link href={'/create'} className='bg-blue-500 px-4 py-2 max-w-20 '>
+//       <button className='cursor-pointer '>Create</button>
+//       </Link>
+//       </div>
+//       <div className='space-y-6 flex flex-wrap gap-2'>
+//         {menuItems.map(item => (
+//           <div key={item.id} className='border rounded-2xl h-[25rem] max-w-[20rem]'>
+//             <Image src={item.image!} alt='' width={320} height={0}
+//             className='rounded-t-2xl '></Image>
+//             <div className='p-4 space-y-12'>
+//           <Link href={`/menu/${item.id}`}
+//           className='text-xl hover:text-blue-500 font-bold'>
+//             {item.name}
+//           </Link> 
+//           <p className='text-sm text-gray-600 '>
+//             {item.description.substring(0, 100)}
+//             </p>
+            
+//             <div className='flex justify-between'>
+//             <p className='text-3xl font-bold'>${item.price}</p>
+//             <button className=' bg-amber-400 px-4 py-2 text-sm font-extrabold hover:bg-amber-300 rounded border-b-3 border-amber-600 cursor-pointer'>Add to Cart</button>
+//             </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
 
 export default function MenuPage() {
     const [items, setItems] = useState<MenuItem[]>([]);
