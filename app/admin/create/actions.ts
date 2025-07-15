0bca209a6c1c99d136347a0469295f5c5cef4e24
Ashley2n <a.windbreacker@gmail.com>
2025-07-15 10:21:29 -0500

    Changed on letter

--- app/admin/create/actions.ts
+++ app/admin/create/actions.ts
@@ -1,51 +1,45 @@
-"use server"
-import { dishFormSchema, DishForm } from "@/lib/zodSchema/zodSchemas"
+"use server";
+import { dishFormSchema, DishForm } from "@/lib/zodSchema/zodSchemas";
 import { db } from "@/lib/db/drizzle";
 import { dishes } from "@/lib/db/schema";
 import { redirect } from "next/navigation";
 
-
 export async function createDish(newDish: unknown) {
-    // Server-Side Validation
-  console.log("Creating Dish Action Called")
+  // Server-Side Validation
+  console.log("Creating Dish Action Called");
 
   const results = dishFormSchema.safeParse(newDish);
   const businessIdInt: number = Number(results.data?.business_id);
-// Checking for any errors
-if (!results.success) {
+  // Checking for any errors
+  if (!results.success) {
     // console.log(results.error.issues)
     let errorMessage = "";
 
     // If we werer to add toast goo way to incoparte error Message.
     results.error.issues.forEach((issue) => {
-        errorMessage =
-            errorMessage + issue.path[0] + ": " + issue.message + ". ";
+      errorMessage = errorMessage + issue.path[0] + ": " + issue.message + ". ";
     });
 
     console.log(results.error);
 
-    return{
-        error: errorMessage,
+    return {
+      error: errorMessage,
     };
-}
+  }
 
-
-
-try{
+  try {
     await db.insert(dishes).values({
-        businessId: businessIdInt,
-        name: results.data.name,
-        active: true,
-        description: results.data.discription,
-        image: results.data.image,
-        price: results.data.price,
+      businessId: businessIdInt,
+      name: results.data.name,
+      active: true,
+      description: results.data.discription,
+      image: results.data.image,
+      price: results.data.price,
     });
-    }
-    catch (err) {
-        console.log(err);
-    };
-    redirect("/menu");
-
+  } catch (err) {
+    console.log(err);
+  }
+  redirect("/menu");
 }
 
 // export {CreateDish}
--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link'
-import CreateDishForm from '@/components/createDishForm'
+import CreateDishForm from '@/components/CreateDishForm'
 import { useActionState, useState } from 'react'
 import { createDish } from './actions'
 
