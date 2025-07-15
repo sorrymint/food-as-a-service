2f82a53f4e209f2b296e8c871d153f8ec7944222
Ashley2n <a.windbreacker@gmail.com>
2025-07-15 10:25:52 -0500

    Undoing the file name

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link'
-import CreateDishForm from '@/components/CreateDishForm'
+import CreateDishForm from '@/components/createDishForm'
 import { useActionState, useState } from 'react'
 import { createDish } from './actions'
 
