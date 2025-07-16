4906d0345f31c72fe753665852390713f3a400aa
Ashley2n <a.windbreacker@gmail.com>
2025-07-16 09:38:27 -0500

    init commit

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -2,12 +2,8 @@
 import Link from 'next/link'
 import CreateDishForm2 from './Components/createDishForm2'
 import CreateDishForm from '@/components/createDishForm'
-import { useActionState, useState } from 'react'
-import { createDish } from './actions'
 
 export default function Create() {
-
-
 
   return (
     <div>
@@ -22,23 +18,8 @@ export default function Create() {
         </Link>
         <h2 className='mb-6 font-extrabold'>Create A New Post</h2>
 
-        <CreateDishForm2/>
+        <CreateDishForm/>
         
     </div>
   )
 }
-  // id: serial('id').primaryKey(),
-  // businessId: serial('business_id')
-  //   .notNull()
-  //   .references(() => businesses.id),
-  // name: varchar('name', {length: 100})
-  //   .notNull()
-  //   .unique(),
-  // description: varchar('description', {length: 500})
-  //   .notNull(),
-  // active: boolean('active')
-  //   .notNull(),
-  // image: text('image_url'),
-  // price: numeric('price'),
-  // createdAt: timestamp('created_at').notNull().defaultNow(),
-  // updatedAt: timestamp('updated_at').defaultNow()
