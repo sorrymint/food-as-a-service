e9a46c595768aaf50f4858238731933623163fd9
sorrymint <ldmatheis.00@gmail.com>
2025-07-23 10:32:42 -0500

    Updated Postgres DB URL. v2 removed comma

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: './lib/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
 
