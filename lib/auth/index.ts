24936c34bd386a1558bef1084bc32886ee13c708
TheClayFerg <claytonferguson55@yahoo.com>
2025-08-03 19:46:16 -0500

    Had to debug, it removed some of my files which I switched over to this branch from previous one, it works now.

---
+++ lib/auth/index.ts
@@ -0,0 +1,11 @@
+﻿import { getSession } from './session';
+
+export async function auth() {
+    const session = await getSession();
+
+    if (!session) {
+        return null;
+    }
+
+    return session;
+}
