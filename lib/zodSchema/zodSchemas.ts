c30cc7f7ee78e128b09f33b919e1f604f4082627
ShimeaG <shimeagbetsi@gmail.com>
2025-08-06 11:22:24 -0500

    Added the zod emun for the dish status

--- lib/zodSchema/zodSchemas.ts
+++ lib/zodSchema/zodSchemas.ts
@@ -1,21 +1,37 @@
 import { z } from "zod";
+import { dishStatus } from "../db/schema";
 
 export const dishFormSchema = z.object({
-    id: z.number().optional(),
-    businessId: z.number().min(1, "Id cant be less than 0"),
-
-    name: z.string()
+    id: z
+    .number()
+    .optional(),
+ 
+    businessId: z
+    .coerce
+    .number()
+    .positive({message: "Must be postive"}),
+ 
+    name: z
+    .string()
     .min(1, "Name field can't be empty"),
-
-    active: z.boolean(),
-
-    description: z.string().min(10, "description must be at least 10 characters")
-    .max(500, "description must be at most 500 characters"), 
-    tags: z.string().nullable(),
-
+ 
+    isActive: z.enum
+    (dishStatus.enumValues),
+ 
+    description: z
+    .string()
+    .min(10, "description must be at least 10 characters")
+    .max(500, "description must be at most 500 characters"),
+ 
     //making this into a URl(add .url())
-    image: z.string().nullable(),
-    price: z.string().min(4, "Enter a valid Price ex: 3.00").nullable()
+    image: z
+    .string()
+    .nullable(),
+ 
+    price: z.coerce
+    .string({message: "Price is required"})
+    .min(4, "Enter a valid Price ex: 3.00")
 })
+ 
 
 export type DishType = z.infer<typeof dishFormSchema>;
